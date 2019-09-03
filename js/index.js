d6ebda83a1e3722e25ea78f9dcac91e181cf8f21
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-03 15:33:55 -0400

    initial commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,81 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Selectors
+
+const tags = document.getElementsByTagName('a');
+console.log(tags);
+
+function add(element, value){
+  element.forEach((e, index) => e.innerText = value[index]);
+}
+// Navigation
+
+let nav = document.querySelector('nav');
+let addFirst = document.createElement('a');
+let addLast = document.createElement('a');
+
+nav.appendChild(addLast);
+nav.prepend(addFirst);
+
+let item = document.querySelectorAll('nav a');
+let values = Object.values(siteContent['nav']);
+
+add(item, values);
+item.forEach((item) => item.style.color = "#green");
+
+// CTA 
+
+let ctaContent = document.querySelector('.cta-text h1');
+ctaContent.innerHTML = siteContent['cta']['h1'];
+
+let ctaButton = document.querySelector('.cta-text button');
+ctaButton.innerText = siteContent['cta']['button'];
+
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]['img-src']);
+
+// Content
+
+let contentHeader = document.querySelectorAll('.text-content h4');
+let contentObjKey = Object.keys(siteContent['main-content']);
+let headers = [];
+
+contentObjKey.forEach((item) => {
+  if (item.includes('h4')){
+    headers.push(siteContent['main-content'][item]);
+  }
+});
+
+add(contentHeader, headers);
+
+let contentParagraphs = document.querySelectorAll('.text-content p');
+let paragraphs = [];
+contentObjKey.forEach((item) => {
+  if (item.includes('content')) {
+    paragraphs.push(siteContent['main-content'][item]);
+  }
+});
+
+
+add(contentParagraphs, paragraphs);
+
+let imageSnippets = document.getElementById('middle-img');
+imageSnippets.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+// Contact
+
+let contactHeader = document.querySelector('.contact h4');
+contactHeader.innerText = siteContent['contact']['contact-h4'];
+
+let contactItem = document.querySelectorAll('.contact p');
+let contactValue = Object.values(siteContent['contact']).splice(1, 3);
+
+add(contactItem, contactValue);
+
+// Footer
+
+let footer = document.querySelector('footer p');
+footer.innerText = siteContent['footer']['copyright'];
+
